88cc375b7caef92ccc4d01f1c41efaa02cc9e02e
Dominik Schwabe <dominik.s01@gmx.de>
2024-04-21 11:31:49 +0200

    Controller initialize implementiert

--- Java2AB2Aufgabe2/src/application/Controller.java
+++ Java2AB2Aufgabe2/src/application/Controller.java
@@ -6,12 +6,92 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.StackPane;
 
+/**
+ * 
+ * Controller zuständig für die CardBorderPane.fxml
+ *
+ * @author Markus Suchalla, Cheng-Fu Ye, Dominik Schwabe
+ */
 public class Controller {
+   
+   /**
+    * Innere Klasse als EventHandler für die Knöpfe.
+    *
+    * Verwaltet die StackPane.
+    *
+    * @author Markus Suchalla, Cheng-Fu Ye, Dominik Schwabe
+    */
+   public class Listener implements EventHandler<ActionEvent> {
 
+      private StackPane pane;
+      private Label label;
+      private String text;
+
+      /**
+       * 
+       * Konstruktor für den EventHandler.
+       *
+       * @param stackPane Auf welcher Stackpane die Elemente gekoppelt werden sollen.
+       * @param label Label, welches den eigentlich Text beinhaltet.
+       * @param text Text, welcher auf der Schicht angezeigt werden soll.
+       */
+      public Listener(StackPane stackPane, String text)
+      {
+         pane=stackPane;
+         this.text = text;
+
+      }
+
+      /**
+       * Eventhandler, welcher die Labels der Stackpane erzeugt und behandelt.
+       */
+      @Override
+      public void handle(ActionEvent arg0)
+      {
+         try {
+            if(!pane.getChildren().isEmpty())
+               pane.getChildren().getLast().setVisible(false);
+
+            if(pane.getChildren().contains(label))
+            {
+               pane.getChildren().remove(label);
+            }
+            else
+            {
+               label = new Label(text);
+               label.setMinSize(200, 100);   
+            }
+
+            pane.getChildren().addLast(label);
+
+            pane.getChildren().getLast().setVisible(true);
+
+
+
+         }catch(Exception e)
+         {
+            Alert alert =
+                  new Alert(AlertType.ERROR, "Fehler nach dem Drücken des Buttons", ButtonType.OK);
+            alert.setResizable(true);
+            alert.showAndWait();
+         }
+
+      }
+
+
+
+   }
+   
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -36,7 +116,13 @@ public class Controller {
         assert button2 != null : "fx:id=\"button2\" was not injected: check your FXML file 'CardBorderPane.fxml'.";
         assert button3 != null : "fx:id=\"button3\" was not injected: check your FXML file 'CardBorderPane.fxml'.";
         assert stackPane != null : "fx:id=\"stackPane\" was not injected: check your FXML file 'CardBorderPane.fxml'.";
-
+        Listener listener1 = new Listener(stackPane,"Knopf 1");
+        button1.setOnAction(listener1);
+        Listener listener2 = new Listener(stackPane,"Knopf 2");
+        button2.setOnAction(listener2);
+        Listener listener3 = new Listener(stackPane,"Knopf 3");
+        button3.setOnAction(listener3);
+        button1.fire();
     }
 
 }
--- Java2AB2Aufgabe2/src/application/Main.java
+++ Java2AB2Aufgabe2/src/application/Main.java
@@ -1,29 +1,24 @@
 package application;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 
-import exception.InvalidSourceException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
 /**
  * 
- * TODO Comment
+ * Klasse zur Darstellung eines Card Layouts.
  *
  * @author Markus Suchalla, Cheng-Fu Ye, Dominik Schwabe
  */
-public class MyApp extends Application
+public class Main extends Application
 {
 
    /**
@@ -38,7 +33,8 @@ public class MyApp extends Application
    }
 
    /**
-    * TODO Comment
+    * Wird durch JavaFX-Runtime nach dem Aufruf der init-Methode aufgerufen.
+    * Initialisert die primaryStage mithilfe des FXMLLoaders mit der CardBorderPane.fxml
     */
    @Override
    public void start(Stage primaryStage) throws Exception
@@ -47,21 +43,20 @@ public class MyApp extends Application
       {
          if(primaryStage == null) 
          {
-            throw new NullPointerException("MyApp.start(Stage PrimaryStage): Ungültige Null-Referenz zu PrimaryStage!");
+            throw new NullPointerException("Main.start(Stage primaryStage): Ungültige Null-Referenz zu PrimaryStage!");
          }
 
-         BorderPane root = (BorderPane) FXMLLoader.load(MyApp.class.getResource("/CardBorderPane.fxml"));
+         BorderPane root = (BorderPane) FXMLLoader.load(Main.class.getResource("/CardBorderPane.fxml"));
          Scene scene = new Scene(root);
          scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
          primaryStage.setScene(scene);
          primaryStage.show();
 
       }
-
       catch (Exception e)
       {
          Alert alert =
-               new Alert(AlertType.ERROR, "Unbekannter Fehler!", ButtonType.OK);
+               new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK);
          alert.setResizable(true);
          alert.showAndWait();
          try 
